77f36d957ab7f8c61ec1d83b7ac7ccd4e1b442e5
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-02-03 14:39:57 -0800

    Added register, login, and logout modules to auth.controller.js in ES7 Format

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,18 +1,71 @@
 const { User } = require("../models/User.model");
+const jwt = require("jsonwebtoken")
 
-module.exports.index = (request, response) => {
+index: (request, response) => {
     response.json({
         message: "Connected!"
     });
 }
 
-module.exports.register = (request, response) => {
+// ES7 Form : async/await
+register: (req, res) => {
     User.create(req.body)
-    .then(user => {
-        res.json({ msg: "success!", user: user });
-    })
-    .catch(err => res.json(err));
+        .then(user => {
+            const userToken = jwt.sign({
+                id: user._id
+            }, process.env.SECRET_KEY);
+
+            res
+                .cookie("usertoken", userToken, secret, {
+                    httpOnly: true
+                })
+                .json({ msg: "success!", user: user });
+        })
+        .catch(err => res.json(err));
 }
+
+// ES7 Form : async/await
+login: async(req, res) => {
+        const user = await User.findOne({ email: req.body.email });
+
+        if(user === null) {
+            // if email not found in users collection
+            return res.sendStatus(400);
+        }
+
+        // if user is found with email address, compare the supplied password to the hashed password in the database
+        const correctPassword = await bcrypt.compare(req.body.password, user.password);
+
+        if(!correctPassword) {
+            // if password wasn't a match!
+            return res.sendStatus(400);
+        }
+
+        // if password was correct
+        const userToken = jwt.sign({
+            id: user._id
+        }, process.env.SECRET_KEY);
+
+        // note: chained calls to cookie and json
+        res
+            .cookie("usertoken", userToken, secret, {
+                httpOnly: true
+            })
+            .json({ msg: "success!" });
+    }
+
+logout: (req, res) => {
+    res.clearCookie('usertoken');
+    res.sendStatus(200);
+}
+
+// module.exports.register = (request, response) => {
+//     User.create(req.body)
+//     .then(user => {
+//         res.json({ msg: "success!", user: user });
+//     })
+//     .catch(err => res.json(err));
+// }
 
 // module.exports.create_HERE = (request, response) => {
 //     const { ATTRIBUTES_HERE } = request.body;
