e2ab70e01c8fbeb48668f767e332ca33d73bd54a
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-02-15 14:31:06 -0800

    Setup Main.jsx with createUser, getUser, and connection status functions and routes

--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -6,29 +6,54 @@ import Form from "../components/Form";
 
 export default () => {
     const [message, setMessage] = useState("Connecting...");
-    const [EXAMPLEs, setEXAMPLEs] = useState([]);
+    const [user, setUser] = useState({});
+    const [users, setUsers] = useState([]);
     const [loaded, setLoaded] = useState(false);
     const [ load, setLoad ] = useState(false);
     const [ errors, setErrors ] = useState(false);
+    const [ login, setLogin ] = useState(false);
 
-    useEffect(()=>{
-        axios.get('http://localhost:8000/api/EXAMPLE')
-            .then(res=>{
-                setEXAMPLEs(res.data);
-                setLoaded(true);
-            });
-    },[])
+    if (login) {
+        useEffect(()=>{
+            axios.get('http://localhost:8000/api/users', { withCredentials: true })
+                .then(res=>{
+                    setUsers(res.data);
+                    setLoaded(true);
+                });
+        },[])
+    }
     useEffect(() => {
         axios.get("http://localhost:8000/api")
             .then(res => setMessage(res.data.message))
     }, []);
 
-    const createEXAMPLE = (EXAMPLE) => {
-        setEXAMPLEs([...EXAMPLEs, example]);
-        axios.post("http://localhost:8000/api/!!!!EXAMPLE!!!!/new", example)
+    const createUser = (user) => {
+        setUsers([...users, user]);
+        axios.post("http://localhost:8000/api/register", user,{ withCredentials: true })
             .then(res => {
                 console.log(res);
-                navigate("/EXAMPLE");
+                navigate("/");
+                setLogin(true);
+            })
+            .catch(err => {
+                const errorResponse = err.response.data.errors; // Get the errors from err.response.data
+                const errorArr = []; // Define a temp error array to push the messages in
+                for (const key of Object.keys(errorResponse)) { // Loop through all errors and get the messages
+                    errorArr.push(errorResponse[key].message)
+                }
+                // Set Errors
+                setErrors(errorArr);
+                setLoad(true);
+            });
+    }
+
+    const getUser = (user) => {
+        axios.get("http://localhost:8000/api/login", user,{ withCredentials: true })
+            .then(res => {
+                console.log(res);
+                setUser(res.data);
+                setLogin(true);
+                navigate("/");
             })
             .catch(err => {
                 const errorResponse = err.response.data.errors; // Get the errors from err.response.data
@@ -43,13 +68,23 @@ export default () => {
     }
 
     const removingDOM = (id) => {
-        setEXAMPLEs(examples.filter(example => example._id != id));
+        setUsers(users.filter(user => user._id != id));
     }
 
     return(
         <>
             <h1>Connection status: {message}</h1>
-            <Form EXAMPLEIn="" submitInput={createEXAMPLE} />
+            <h2> Logged In Status: {
+                login ?
+                "No One!" :
+                user.firstName, user.lastName
+            } </h2>
+            
+            {
+                login ?
+                <Form logged={login} submitInput={createUser} /> :
+                <Form logged={login} submitInput={getUser} />
+            }
             {
                 load ?
                 <p style={{color: "red"}}>{errors}</p> :
