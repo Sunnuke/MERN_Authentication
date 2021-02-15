c6a1e03a85b1b3fbbe7827aec723949b776be13b
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-02-15 15:01:47 -0800

    Updated Form.jsx with state attributes to store form info and pass it back as an object for login or register

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,22 +1,57 @@
 import React, { useState } from "react";
 
 export default (props) => {
-    const { EXAMPLEIn,  submitInput } = props;
-    const [ EXAMPLE, setEXAMPLE ] = useState(EXAMPLEIn);
+    const { logged,  submitInput } = props;
+    const [ firstName, setFirstName ] = useState("");
+    const [ lastName, setLastName ] = useState("");
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const [ confirmPassword, setConfirmPassword ] = useState("");
 
     const Process = (e) => {
         e.preventDefault();
-        submitInput({ EXAMPLE });
+        submitInput({ firstName, lastName, email, password, confirmPassword });
     }
 
     return(
         <div>
             <form onSubmit={Process}>
-                <label>EXAMPLE:</label>
+                { 
+                    logged ?
+                    "" :
+                    <>
+                        <label>First Name:</label>
+                        <input type="text" 
+                        name="firstName"
+                        value={firstName}
+                        onChange={ e => { setFirstName(e.target.value) } } /><br/>
+
+                        <label>Last Name:</label>
+                        <input type="text" 
+                        name="lastName"
+                        value={lastName}
+                        onChange={ e => { setLastName(e.target.value) } } /><br/>
+                    </>
+                }
+
+                <label>Email:</label>
                 <input type="text" 
-                name="EXAMPLE"
-                value={EXAMPLE}
-                onChange={ e => { setEXAMPLE(e.target.value) } } /><br/>
+                name="email"
+                value={email}
+                onChange={ e => { setEmail(e.target.value) } } /><br/>
+                
+                <label>Password:</label>
+                <input type="text" 
+                name="password"
+                value={password}
+                onChange={ e => { setPassword(e.target.value) } } /><br/>
+                
+                <label>Confirm Password:</label>
+                <input type="text" 
+                name="confirmPassword"
+                value={confirmPassword}
+                onChange={ e => { setConfirmPassword(e.target.value) } } /><br/>
+                
                 <input type="submit" />
             </form>
         </div>
